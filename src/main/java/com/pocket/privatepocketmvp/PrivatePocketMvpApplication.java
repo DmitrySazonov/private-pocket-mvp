2e13b9a4685b302b7b896afa4ce44b7852d11671
DmitrySazonov <dmsazonov@gmail.com>
2021-04-02 12:48:50 +0300

    renaming packages

--- src/main/java/com/pocket/privatepocketmvp/PrivatePocketMvpApplication.java
+++ src/main/java/com/pocket/privatepocketmvp/PrivatePocketMvpApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrivatePocketApplication {
+public class PrivatePocketMvpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrivatePocketApplication.class, args);
+		SpringApplication.run(PrivatePocketMvpApplication.class, args);
 	}
 
 }
